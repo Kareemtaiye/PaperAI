e589dea4e33a5d1cfe837eee45dc7f05e4d63a4e
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-17 05:25:30 +0100

    add: regsiter and find_user_by_email repo added

--- app/repositories/auth_repo.py
+++ app/repositories/auth_repo.py
@@ -1,3 +1,18 @@
-from app.db.session import get_db
+from sqlalchemy import select
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models.user import User
+from app.schemas.auth import UserLoginInput
+
 
 class AuthRepository:
+    async def create_user(self, db: AsyncSession, user: UserLoginInput):
+        db.add(user)
+        await db.commit()
+        await db.refresh(user)
+        return user
+
+    async def find_user_by_email(aelf, db: AsyncSession, email: str):
+        result = await db.execute(select(User).where(User.email == email))
+        return result.scalar_one_or_none()
