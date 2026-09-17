bade71a0532c4acd1bf42adc375f714ba7198903
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-17 05:23:26 +0100

    update: added email_verified col

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -11,6 +11,7 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, nullable=False, index=True)
     password_hash = Column(String, nullable=False)
+    email_verified = Column(Boolean, default=False)
     is_acive = Column(Boolean, default=True)
     is_demo = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=text("now()"))
