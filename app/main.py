ac84f5f957d51cdffe153be38caa2ce15e1458f4
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-17 23:39:48 +0100

    ...:

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.routers import auth
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
+
+app.include_router(auth.router, prefix="/api/v1")
 
 
 @app.get("/health")
