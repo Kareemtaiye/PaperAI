1ad26506b211d1dfa072045fc31a1b4d8f314b43
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-17 05:24:22 +0100

    add: user auth schema for register and token endpoints

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -2,12 +2,11 @@ from pydantic import UUID1, BaseModel
 
 
 class BaseUser(BaseModel):
-    full_name: str
     email: str
 
 
 class UserLoginInput(BaseUser):
-    password: str
+    password_hash: str
 
 
 class UserLoginOuput(BaseUser):
@@ -15,3 +14,5 @@ class UserLoginOuput(BaseUser):
     role: str = "user"
     email_verified: bool
     is_demo: bool
+    created_at: str
+    updated_at: str
