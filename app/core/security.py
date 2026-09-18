12a2e9e929b83b4feb176347c7f8e5c715cbe4b7
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-18 08:39:09 +0100

    add: hash/verify password, generate/verify access token

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,26 @@
 from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+import jwt
+from app.core.config import settings
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = PasswordHash.recommended().hash("dummy_hash")
+
+
+def hash_password(password: str):
+    return password_hash.hash(password)
+
+
+def verify_password(password: str, hash: str):
+    return password_hash.verify(password, hash)
+
+
+def generate_access_token(user_id: str):
+    return jwt.encode({"payload": user_id}, settings.secret_key, algorithm="HS256")
+
+
+def verify_access_token(token: str):
+    return jwt.decode(token, settings.secret_key, algorithm="HS256")
