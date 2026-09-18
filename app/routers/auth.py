05a8786c228a2ea79ddd93717f212e78e1da9747
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-18 08:40:45 +0100

    add: register auth endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Response
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from app.db.session import get_db
-from app.schemas.auth import RegisterInput
+from app.schemas.auth import RegisterInput, UserResponse
 from app.services.auth_service import AuthService
 
 service = AuthService()
@@ -11,11 +13,11 @@ service = AuthService()
 router = APIRouter(prefix="/auth")
 
 
-@router.post("/register")
+@router.post("/register", response_model=UserResponse, response_model_exclude_none=True)
 async def create_user(user: RegisterInput, db=Depends(get_db)):
     user = await service.register(db=db, user=user)
 
-    return {"data": "created"}
+    return {"status": "success", "data": user}
 
 
 @router.post("/token", tags=["token"])
