ce2b29fcc140e0e23dea1c4c5b0c518c004015e5
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-18 16:17:58 +0100

    add, update: login and register

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,8 +1,13 @@
-from app.core.security import hash_password
+from app.core.security import (
+    DUMMY_HASH,
+    generate_access_token,
+    hash_password,
+    verify_password,
+)
 from app.repositories.auth_repo import AuthRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.auth import RegisterInput
+from app.schemas.auth import LoginInput, RegisterInput
 
 
 class AuthService:
@@ -22,3 +27,23 @@ class AuthService:
             return None  # --- im raising UserNotFoundException later
 
         return user
+
+    async def login(self, db: AsyncSession, user_data: LoginInput):
+        user = await self.repo.find_user_by_email(db=db, email=user_data.username)
+
+        if not user:
+            verify_password(user_data.username, DUMMY_HASH)  # for timing attack
+            return None
+
+        if not user.is_acive:
+            # Logger warning later
+            return None
+
+        if not verify_password(user_data.password, user.password_hash):
+            return None
+
+        # Create session later
+
+        access_token = generate_access_token(str(user.id))
+        # refresh token later - for managing Session later
+        return access_token
