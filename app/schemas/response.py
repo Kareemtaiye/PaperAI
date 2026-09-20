492eb94eee8ebf362f61ff9c2935860a3c16f080
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-20 18:32:19 +0100

    update: added details attr to err reponse

--- app/schemas/response.py
+++ app/schemas/response.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict
 
 
@@ -5,5 +7,6 @@ class ErrorResponse(BaseModel):
     status: str
     message: str
     code: int
+    details: Optional[dict] = None
 
     model_config = ConfigDict(from_attributes=True)
