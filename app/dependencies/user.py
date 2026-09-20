8858c1a2bad0113ddcb15a0ef2f2f2c276b9a0b4
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-20 18:28:47 +0100

    update: removed jsonable_encoder

--- app/dependencies/user.py
+++ app/dependencies/user.py
@@ -1,6 +1,5 @@
 from typing import Annotated
 
-from fastapi.encoders import jsonable_encoder
 from app.core.security import oauth_scheme, verify_jwt
 from fastapi import Depends, HTTPException, status
 
@@ -19,12 +18,10 @@ async def get_current_user(
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=jsonable_encoder(
-                ErrorResponse(
-                    status="error",
-                    code=status.HTTP_401_UNAUTHORIZED,
-                    message="Not authenticated",
-                )
+            detail=ErrorResponse(
+                status="error",
+                code=status.HTTP_401_UNAUTHORIZED,
+                message="Not authenticated",
             ),
         )
 
@@ -35,12 +32,10 @@ async def get_current_user(
     if not user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=jsonable_encoder(
-                ErrorResponse(
-                    status="error",
-                    code=status.HTTP_400_BAD_REQUEST,
-                    message="Invalid access token",
-                )
+            detail=ErrorResponse(
+                status="error",
+                code=status.HTTP_400_BAD_REQUEST,
+                message="Invalid access token",
             ),
         )
 
