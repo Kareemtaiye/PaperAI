3aeb5b29932930bedde08002f9184f2e85a1593b
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-20 10:14:01 +0100

    get_current_user implemented

--- app/dependencies/user.py
+++ app/dependencies/user.py
@@ -5,12 +5,16 @@ from app.core.security import oauth_scheme, verify_jwt
 from fastapi import Depends, HTTPException, status
 
 from app.db.session import get_db
+from app.schemas.auth import UserOuput
 from app.schemas.response import ErrorResponse
+from app.services.user_service import UserService
+
+service = UserService()
 
 
 async def get_current_user(
     token: Annotated[str, Depends(oauth_scheme)], db=Depends(get_db)
-):
+) -> UserOuput:
     if not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -23,6 +27,22 @@ async def get_current_user(
             ),
         )
 
-    # payload = verify_jwt(token)
+    payload = verify_jwt(token)
 
-    # user = await
+    user = await service.find_user_by_id(db, payload.get("sub"))
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                jsonable_encoder(
+                    ErrorResponse(
+                        status="error",
+                        code=status.HTTP_400_BAD_REQUEST,
+                        message="Invalid access token",
+                    )
+                )
+            },
+        )
+
+    return user
