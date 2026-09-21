b9ca8f0a9be2113ce56ae330d5ba79871a998097
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-21 15:44:49 +0100

    add: get/delete_[all]_user_paper[s] repo funcs added

--- app/repositories/paper_repo.py
+++ app/repositories/paper_repo.py
@@ -1,5 +1,12 @@
+from uuid import UUID
+
+from sqlalchemy import select, delete
+from sqlalchemy.orm import selectinload
+
 from app.db.models.paper import Paper
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models.user_paper import UserPaper
 
 
 class PaperRepostiory:
@@ -10,3 +17,39 @@ class PaperRepostiory:
         await db.commit()
         await db.refresh(paper)
         return paper
+
+    async def get_all_user_papers(self, db: AsyncSession, user_id: str | UUID):
+        result = await db.execute(
+            select(UserPaper)
+            .where(UserPaper.user_id == user_id)
+            .options(selectinload(UserPaper.paper))
+            .order_by(UserPaper.created_at)
+        )
+
+        return result.scalars().all()
+
+    async def get_user_paper(
+        self, db: AsyncSession, user_id: str | UUID, paper_id: str | UUID
+    ):
+        result = await db.execute(
+            select(UserPaper)
+            .where(UserPaper.user_id == user_id)
+            .where(UserPaper.paper_id == paper_id)
+            .options(selectinload(UserPaper.paper))
+        )
+
+        return result.scalar_one_or_none()
+
+    async def delete_user_paper(
+        self, db: AsyncSession, user_id: str | UUID, paper_id: str | UUID
+    ):
+        result = await db.execute(
+            delete(UserPaper)
+            .where(UserPaper.user_id == user_id)
+            .where(UserPaper.paper_id == paper_id)
+            .returning(UserPaper.id)
+        )
+
+        await db.commit()
+        deleted = result.fetchone()
+        return delete is not None
