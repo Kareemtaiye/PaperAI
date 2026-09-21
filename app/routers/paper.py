75ad83d79e10810a47744eaf843592565db9c2cb
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-21 15:46:09 +0100

    add: get/delete_[all]_user_paper[s] routes implemented

--- app/routers/paper.py
+++ app/routers/paper.py
@@ -1,30 +1,30 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+
+from fastapi import APIRouter, Body, Depends, Response, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from app.db.session import get_db
 from app.dependencies.user import get_current_user
+from app.schemas import paper
 from app.schemas.paper import PaperManualCreate, PaperResponse
 from app.schemas.response import ErrorResponse
 from app.services.paper_service import PaperService
+from app.utils.mappers import build_paper_response
 
 router = APIRouter(prefix="/paper", tags=["Paper"])
 service = PaperService()
 
 
-@router.post("/manual")
+@router.post("/manual", response_model=PaperResponse)
 async def create_paper_manual(
     data: PaperManualCreate, db=Depends(get_db), current_user=Depends(get_current_user)
 ):
-    if not data.title:
-        raise HTTPException(
-            status_code=400,
-            detail=ErrorResponse(status="error", code=401, message="No title provided"),
-        )
-
     paper, user_paper = await service.create_paper_manual(
         db=db, title=data.title, user_id=current_user.id
     )
 
-    return PaperResponse(
+    res_data = PaperResponse(
         id=paper.id,
         title=paper.title,
         source=paper.source,
@@ -32,10 +32,50 @@ async def create_paper_manual(
         notes=user_paper.notes,
         progress=user_paper.progress,
         created_at=user_paper.created_at,
-        # rest of fields null for manual paper
         abstract=None,
         authors=None,
         categories=None,
         arxiv_id=None,
         published_at=None,
     )
+    return JSONResponse(jsonable_encoder({"status": "success", "data": res_data}))
+
+
+@router.get("/", response_model=list[PaperResponse])
+async def get_all_user_papers(
+    db=Depends(get_db), current_user=Depends(get_current_user)
+):
+    user_papers = await service.get_all_user_papers(db=db, user_id=current_user.id)
+
+    data = [build_paper_response(paper) for paper in user_papers]
+    return JSONResponse(jsonable_encoder({"status": "success", "data": data}))
+
+
+@router.get("/{paper_id}", response_model=PaperResponse)
+async def get_user_paper(
+    paper_id: str,
+    db=Depends(get_db),
+    current_user=Depends(get_current_user),
+):
+    user_paper = await service.get_user_paper(
+        db=db, user_id=current_user.id, paper_id=paper_id
+    )
+
+    return JSONResponse(
+        jsonable_encoder(
+            {"status": "success", "data": build_paper_response(user_paper)}
+        )
+    )
+
+
+@router.delete("/{paper_id}", response_model=PaperResponse)
+async def delete_user_paper(
+    paper_id: str,
+    db=Depends(get_db),
+    current_user=Depends(get_current_user),
+):
+    Paper = await service.delete_user_paper(
+        db=db, user_id=current_user.id, paper_id=paper_id
+    )
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
