30141ce829ff15646568e0d6396deb985e6a2884
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-21 15:45:20 +0100

    add: get/delete_[all]_user_paper[s] services implemented

--- app/services/paper_service.py
+++ app/services/paper_service.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from app.exceptions.resource_not_found import PaperNotFoundException
 from app.repositories.paper_repo import PaperRepostiory
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -16,16 +17,37 @@ class PaperService:
         self, db: AsyncSession, title: str, user_id: str | UUID
     ):
 
-        async with db.begin():
-            paper = await self.repo.create_paper_manual(
-                db=db, title=title, source="manual"
-            )
+        paper = await self.repo.create_paper_manual(db=db, title=title, source="manual")
 
-            user_paper = await user_paper_service.create_user_paper(
-                db=db, user_id=user_id, paper_id=paper.id
-            )
+        user_paper = await user_paper_service.create_user_paper(
+            db=db, user_id=user_id, paper_id=paper.id
+        )
 
         await db.refresh(paper)
         await db.refresh(user_paper)
 
         return paper, user_paper
+
+    async def get_all_user_papers(self, db: AsyncSession, user_id: str | UUID):
+        return await self.repo.get_all_user_papers(db=db, user_id=user_id)
+
+    async def get_user_paper(
+        self, db: AsyncSession, user_id: str | UUID, paper_id: str | UUID
+    ):
+        paper = await self.repo.get_user_paper(
+            db=db, user_id=user_id, paper_id=paper_id
+        )
+
+        if not paper:
+            raise PaperNotFoundException(paper_id)
+        return paper
+
+    async def delete_user_paper(
+        self, db: AsyncSession, user_id: str | UUID, paper_id: str | UUID
+    ):
+        paper = await self.repo.delete_user_paper(
+            db=db, user_id=user_id, paper_id=paper_id
+        )
+
+        if not paper:
+            raise PaperNotFoundException(paper_id)
