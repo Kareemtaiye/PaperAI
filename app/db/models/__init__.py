3b2b3d186f3fcc639c763f6fb1c251d30297725d
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-24 08:04:30 +0100

    remove redundant import

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -5,5 +5,4 @@ from app.db.models.user_paper import UserPaper
 from app.db.models.analysis import PaperAnalysis
 from app.db.models.relationship import PaperRelationship
 from app.db.models.digest import Digest
-from app.db.models.refresh_token import RefreshToken
 from app.db.models.task import Task
