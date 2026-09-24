554b920576bce8873545121fdf77f9ed7be22c27
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-24 08:03:57 +0100

    add paper import and task routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.exceptions.handlers import register_exception_handler
-from app.routers import auth, paper
+from app.routers import auth, paper, paper_import, task
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
 register_exception_handler(app)
@@ -13,4 +13,6 @@ async def health():
 
 
 app.include_router(auth.router, prefix="/api/v1")
+app.include_router(paper_import.router, prefix="/api/v1")
 app.include_router(paper.router, prefix="/api/v1")
+app.include_router(task.router, prefix="/api/v1")
