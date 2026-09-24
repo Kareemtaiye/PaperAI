856d07e1f4edc08952a2071bd9e34485a110c4b1
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-24 08:05:20 +0100

    fix user_paper  relationship issue

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -23,10 +23,12 @@ class User(Base):
     user_papers = relationship(
         "UserPaper", back_populates="user", cascade="all, delete-orphan"
     )
-    digest = relationship("Digest", back_populates="user", cascade="all, delete-orphan")
+    digests = relationship(
+        "Digest", back_populates="user", cascade="all, delete-orphan"
+    )
 
     refresh_tokens = relationship(
         "RefreshToken", back_populates="user", cascade="all, delete-orphan"
     )
 
-    task = relationship("Task", back_populates="user", cascade="all, delete-orphan")
+    tasks = relationship("Task", back_populates="user", cascade="all, delete-orphan")
