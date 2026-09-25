19e1caeaffabfb8c1841dfc39241e4abf177dffd
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-25 09:49:00 +0100

    add websocket router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.exceptions.handlers import register_exception_handler
-from app.routers import auth, paper, paper_import, task
+from app.routers import auth, paper, paper_import, task, websocket
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
 register_exception_handler(app)
@@ -16,3 +16,4 @@ app.include_router(auth.router, prefix="/api/v1")
 app.include_router(paper_import.router, prefix="/api/v1")
 app.include_router(paper.router, prefix="/api/v1")
 app.include_router(task.router, prefix="/api/v1")
+app.include_router(websocket.router, prefix="/api/v1")
